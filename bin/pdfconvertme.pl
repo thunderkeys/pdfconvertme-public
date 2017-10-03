dd3360ce45306fad251f9d8b28e4d3c5fee6e619
Brian Almeida <bma@thunderkeys.net>
2017-10-03 08:08:50 -0400

    Update copyright date

--- bin/pdfconvertme.pl
+++ bin/pdfconvertme.pl
@@ -3,7 +3,7 @@
 # License:
 # The MIT License (MIT)
 #
-# Copyright (c) 2013-2014 Brian Almeida <bma@thunderkeys.net>
+# Copyright (c) 2013-2017 Brian Almeida <bma@thunderkeys.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
