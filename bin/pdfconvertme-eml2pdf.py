9bdbb7a19041b6feb7aeef86c3dab6850b0eb584
Brian Almeida <bma@thunderkeys.net>
2020-03-14 13:50:37 -0400

    bump copyright years

--- bin/pdfconvertme-eml2pdf.py
+++ bin/pdfconvertme-eml2pdf.py
@@ -3,7 +3,7 @@
 # License:
 # The MIT License (MIT)
 #
-# Copyright (c) 2013-2018 Brian Almeida <bma@thunderkeys.net>
+# Copyright (c) 2013-2020 Brian Almeida <bma@thunderkeys.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
